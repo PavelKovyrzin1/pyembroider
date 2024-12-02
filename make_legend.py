eee6adc4450f48868c9de87a05305811a4c78988
Ваше Имя <you@example.com>
2024-12-02 23:58:28 +0300

    new pixelate and upgrade legend

--- make_legend.py
+++ make_legend.py
@@ -17,10 +17,10 @@ def make_legend_image(image):
             unique_colors.add(rgb)
 
     # Создаем изображение легенды
-    square_size = 100  # размер квадрата
+    square_size = 75  # размер квадрата
     padding = 10       # отступ между квадратами
-    image_width = 800  # ширина изображения
-    image_height = 600  # высота изображения
+    image_width = 1920   # ширина изображения
+    image_height = 1080   # высота изображения
 
     # Создание нового изображения
     image = Image.new("RGB", (image_width, image_height), "white")
@@ -28,31 +28,34 @@ def make_legend_image(image):
 
     # Шрифт для текста
     try:
-        font = ImageFont.truetype("arial.ttf", 16)
+        font = ImageFont.truetype("arial.ttf", 10)
     except IOError:
         font = ImageFont.load_default()
 
     # Рисуем квадраты с цветами и их названиями
     x, y = padding, padding
-    for color_name, color_info in floss_colors['DMC'].items():
-        rgb = color_info['rgb']
-        if not rgb in unique_colors:
-            continue
-        # Рисуем цветной квадрат
-        draw.rectangle([x, y, x + square_size, y + square_size], fill=rgb)
+    for brand in floss_colors:
+        draw.text((x + 5, y + 5), brand, fill="black", font=font)
+        y += square_size + padding
+        for color_name, color_info in floss_colors[brand].items():
+            rgb = color_info['rgb']
+            if not rgb in unique_colors:
+                continue
+            # Рисуем цветной квадрат
+            draw.rectangle([x, y, x + square_size, y + square_size], fill=rgb)
 
-        # Рисуем название цвета
-        draw.text((x + 5, y + square_size + 5), color_name, fill="black", font=font)
+            # Рисуем название цвета
+            draw.text((x + 5, y + square_size + 5), color_info['code'], fill="black", font=font)
 
-        # Обновляем координаты для следующего квадрата
-        x += square_size + padding
-        if x + square_size > image_width:
-            x = padding
-            y += square_size + padding + 20  # 20 - высота текста
-        
-        # Проверка превышения высоты
-        if y + square_size > image_height:
-            break  # Прекращаем, если не хватает места
-    
+            # Обновляем координаты для следующего квадрата
+            x += square_size + padding
+            if x + square_size > image_width:
+                x = padding
+                y += square_size + padding + 50 # 20 - высота текста
+            # Проверка превышения высоты
+            if y + square_size > image_height:
+                break  # Прекращаем, если не хватает места
+        y += square_size + padding + 50
+        x = padding    
     # Преобразуем множество в список и возвращаем его
     return image
--- pixelate.py
+++ pixelate.py
@@ -6,12 +6,22 @@ def closest_color(requested_color, available_colors):
     Находит ближайший цвет из списка available_colors для заданного цвета requested_color.
     """
     r, g, b = requested_color
-    closest_color = min(available_colors, key=lambda color: (color[0] - r) ** 2 + (color[1] - g) ** 2 + (color[2] - b) ** 2)
+    closest_color = min(available_colors,
+                        key=lambda color: (color[0] - r) ** 2 + (color[1] - g) ** 2 + (color[2] - b) ** 2)
     return closest_color
 
 
-def pixelate(image, available_colors, pixel_size=9):
+def pixelate(image, available_colors, margin_thickness=1):
     margin_color = (0, 0, 0)
+
+    min_size = min(image.size[0], image.size[1])
+
+    if min_size < 700:
+        k = 700 // min_size
+        image = image.resize((image.size[0] * k, image.size[0] * k), Image.NEAREST)
+        min_size *= k
+
+    pixel_size = min_size // 142 + 2
 
     # Уменьшение и увеличение изображения для пикселизации
     image = image.resize((image.size[0] // pixel_size, image.size[1] // pixel_size), Image.NEAREST)
@@ -29,7 +39,17 @@ def pixelate(image, available_colors, pixel_size=9):
     # Рисуем черную границу между пикселями
     for i in range(0, image.size[0], pixel_size):
         for j in range(0, image.size[1], pixel_size):
-            for r in range(pixel_size):
+            # Рисуем границу по горизонтали
+            for r in range(margin_thickness):
+                if i + r < image.size[0] and j < image.size[1]:
+                    pixel[i + r, j] = margin_color
+                if j + r < image.size[1] and i < image.size[0]:
+                    pixel[i, j + r] = margin_color
+
+    # Рисуем черную границу между пикселями
+    for i in range(0, image.size[0], 10 * pixel_size):
+        for j in range(0, image.size[1], 10 * pixel_size):
+            for r in range(10 * pixel_size):
                 if i + r < image.size[0] and j < image.size[1]:
                     pixel[i + r, j] = margin_color
                 if i < image.size[0] and j + r < image.size[1]:
